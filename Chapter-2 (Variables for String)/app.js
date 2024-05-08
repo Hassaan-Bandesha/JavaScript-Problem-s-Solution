5d9ebfb3a9412c78f983b2f4625f39658f3b9c4b
Hassaan-Bandesha <hassaan4742@gmail.com>
2024-05-08 23:37:37 +0500

    Variables for string file solved

---
+++ Chapter-2 (Variables for String)/app.js
@@ -0,0 +1,38 @@
+// Q-1 : Declare a variable called username.
+// var username; 
+
+// Q-2 : Declare a variable called username and assign it to a string that represents your full name.
+// var myName = 'Hassaan Shakeel';
+
+                    // Q-3 : Write script to
+// Q(a) : Declare a JS variable tittled message.
+// var message 
+
+// Q(b) : Assign "Hello World" to varibale message.
+// message = 'Hello World';
+
+// Q(c) : Display the messagein alert box.
+// alert(message);
+
+// Q-4 : Write a script to save student's bio data in JS variables and show the data in alert boxes.
+// var name = 'John Doe';
+// var age = '15 years old';
+// var certification = 'Certified Mobile Applicaton Developmenet';
+// alert(name);
+// alert(age);
+// alert(certification);
+
+// Q-5 : Write a script to display the following alert using one JS variable.
+// 
+                    
+// Q-6 : Declare a variable called Email and assign it to a string that represent your Email Address and show the following message in alert box.
+// var email = 'hassaan4742@email.com';
+// alert('My email address is' + ' ' + email);
+
+// Q-7 : Declare a variable called Book and give it the value "A smarter way to learn JavaScript" and display the following message in slert box. 
+// var book = 'A smarter way to learn JavaScript.';
+// alert("I'm trying to learn from the Book" + " " + book);
+
+// Q-8 : Write a script to display this in browser through JavaScript.
+// var para = document.querySelector('#para');
+// para.innerHTML = 'Yah! I can write HTML content through JavaScript';
