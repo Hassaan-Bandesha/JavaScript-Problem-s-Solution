3fc4c34f0b20054b5b5e5433dc72ed3c24585dfb
Hassaan-Bandesha <hassaan4742@gmail.com>
2024-05-08 23:06:44 +0500

    Alart file solved

---
+++ Chapter-1 (Alert)/app.js
@@ -0,0 +1,21 @@
+// Q-1 : Whrita a script to greet your website visitor using JS alert box.
+// alert('Hello User! Welcome to our website.'); 
+
+// Q-2 : Write a script to display following message on your web page.
+// alert('Error! Please enter a valid password.');
+
+// Q-3 : Write a script to display following message on your web page.
+// alert('Welcome to JS land...\n Happy Coding!');      \n For line break
+
+// Q-4 : Write a scipt to display following message in sequence. 
+// alert('Welcome to JS land');
+// alert('Happy Coding!');
+
+// Q-5 : Generate the following mesage through browser's developer console.
+// alert('Hello... I can run JS through my web browsers console');
+
+// Q-6 : Make use of alerts in your new/existing HTML & CSS project. 
+// Use alert in your HTML CSS project.
+
+// Q-7 : Practice placement of <script> element in head and body section of your project. 
+// Use script tag in head and body.
