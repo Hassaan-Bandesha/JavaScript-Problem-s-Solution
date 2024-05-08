cc746d3933e2c013c7413198a8c2d3a2f9cda269
Hassaan-Bandesha <hassaan4742@gmail.com>
2024-05-08 23:59:39 +0500

    Variables for numbers file solved

---
+++ Chapter-3 (Variables for number)/app.js
@@ -0,0 +1,28 @@
+// Q-1 : Declare a variable called age and assign it to your age and show your age in alert box.
+// const age = 19;
+// alert(`I am ${age} years old`);
+
+// Q-2 : Decalare and initialize a variable to keep tarcl of how many times a visitor visited a web page.Show his/her number of visits on your web page.
+// 
+
+//  Q-3 : Declare a variable called birth year and assign it to your birth year and show the folloeing message in your browser.
+// const birthYear = 2005;
+// const para1 = document.querySelector('#para1');
+// const para2 = document.querySelector('#para2');
+// para1.innerHTML = `My birth year is ${birthYear}`;
+// para2.innerHTML = `Data tyoe of of declared variable is Number.`
+
+// Q-4 : A visitor visited a online clothing store. Write a script to store in variables the following information.
+// const userName = document.querySelector('#userName');
+// const product = document.querySelector('#product');
+// const quantity = document.querySelector('#quantity');
+// const para = document.querySelector('#para');
+
+
+// function clothingBrand(){
+//         para.innerHTML = `${userName.value} ordered ${quantity.value} ${product.value} on Outfitters.`
+
+//     userName.value = '';
+//     product.value = '';
+//     quantity.value = '';
+// }
